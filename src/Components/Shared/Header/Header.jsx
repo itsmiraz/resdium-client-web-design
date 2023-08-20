1d0f857654a3e3ff678817df27d3af6cfc32a2c4
unknown <mirajulislamofficial044@gmail.com>
2023-08-20 19:59:33 +0600

    font, layout, general css adde

---
+++ src/Components/Shared/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../../Assets/Logos/resdium-logo.webp'
+import Image from 'next/image';
+
+
+const Header = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
-
+import Image from "next/image";
+import logo from "../Assets/Logos/resdium-logo.webp";
 
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center animate-pulse">
-      <h1 className="text-center text-5xl font-semibold ">Hello World!</h1>
+      <div>
+        <Image src={logo} alt="resdium-logo" className="w-40 mx-auto my-4"  />
+        <h1 className="text-center text-5xl font-semibold ">Hello World!</h1>
+      </div>{" "}
     </div>
-  )
+  );
 }
