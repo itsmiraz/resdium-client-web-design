04b8d6ce38760114deb9f267b96999acc0a4858f
unknown <mirajulislamofficial044@gmail.com>
2023-08-21 11:01:40 +0600

    header added

--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -5,8 +5,27 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <div>
-        </div>
+        <header className='flex justify-between items-center py-8 px-10 bg-[#F4F4F4]'>
+            <div>
+                <Image src={logo} alt='resdium-logo' className='w-40 ' />
+            </div>
+            <div>
+                <ul className='flex items-center gap-10 font-medium'>
+                    <li>
+                        <p className='cursor-pointer'>HOME</p>
+                    </li>
+                    <li>
+                        <p className='cursor-pointer'>SERVICES</p>
+                    </li>
+                    <li>
+                        <p className='cursor-pointer'>ABOUT</p>
+                    </li>
+                    <li>
+                        <p className='cursor-pointer'>CONTACT</p>
+                    </li>
+                </ul>
+            </div>
+        </header>
     );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center animate-pulse">
       <div>
-        <Image src={logo} alt="resdium-logo" className="w-40 mx-auto my-4"  />
+        {/* <Image src={logo} alt="resdium-logo" className="w-40 mx-auto my-4"  /> */}
         <h1 className="text-center text-5xl font-semibold ">Hello World!</h1>
       </div>{" "}
     </div>
