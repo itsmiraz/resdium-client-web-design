8fcfdcc47399ddaf5ee1e64e9c87ff903eb62517
unknown <mirajulislamofficial044@gmail.com>
2023-08-21 12:58:46 +0600

    new changes

--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
 
             {/* Menus */}
             <div
-                className={`lg:static absolute  ${open ? "left-0 scale-100" : "left-[-1000px] scale-0"} lg:scale-100 transition-all duration-300 ease-in-out top-16 lg:h-full h-screen flex justify-center items-center lg:w-fit w-full bg-[#F4F4F4] `}>
+                className={`lg:static absolute  ${open ? "left-0 scale-100" : "left-[-1000px] scale-0"} lg:scale-100 transition-all duration-300 ease-in-out top-14 lg:h-full h-screen flex justify-center items-center lg:w-fit w-full bg-[#F4F4F4] `}>
                 <ul className='flex lg:flex-row flex-col items-center gap-6 lg:gap-10 font-medium bg-[#F4F4F4]'>
                     <li>
                         <p className='cursor-pointer'>HOME</p>
