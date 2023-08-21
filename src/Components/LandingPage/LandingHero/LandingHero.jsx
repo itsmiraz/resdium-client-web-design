74e4a8528754411c4c9d1453bbaa7b4ee7e1fe3f
unknown <mirajulislamofficial044@gmail.com>
2023-08-21 12:50:14 +0600

    hero responsived

--- src/Components/LandingPage/LandingHero/LandingHero.jsx
+++ src/Components/LandingPage/LandingHero/LandingHero.jsx
@@ -9,9 +9,9 @@ import { Balancer } from 'react-wrap-balancer';
 const LandingHero = () => {
     return (
         <MainContainer>
-            <div className='flex justify-between items-center gap-20'>
-                <div className='w-[60%] space-y-8'>
-                    <h1 className='text-[64px] font-bold leading-[72px] bg-clip-text text-transparent bg-gradient-to-r from-[#3C6E71] to-[#01437C]'>
+            <div className='flex md:flex-row flex-col-reverse justify-between items-center gap-10 md:gap-20'>
+                <div className='w-[100%] md:w-[60%] space-y-8 md:text-start text-center'>
+                    <h1 className='text-[40px] leading-[46px] lg:text-[64px] font-bold lg:leading-[72px]  bg-clip-text text-transparent bg-gradient-to-r from-[#3C6E71] to-[#01437C]'>
                         {HeroConstants.title}
                     </h1>
                     <p className='text-[#666] text-[16px] font-medium'>
@@ -23,7 +23,7 @@ const LandingHero = () => {
                     </p>
                     <MAINBTN title={HeroConstants.cta} />
                 </div>
-                <div className=''>
+                <div className='md:w-fit w-[70%]'>
                     <Image lazy='false' loading='eager' width={400} height={400} src={HeroConstants.heroimg} alt={HeroConstants.heroImgAlt} />
                 </div>
             </div>
--- src/Components/Shared/MainContainer/MainContainer.jsx
+++ src/Components/Shared/MainContainer/MainContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainContainer = ({children}) => {
     return (
-        <div className='h-fit py-20 flex flex-col justify-center items-center px-20'>
+        <div className='h-fit py-20 flex flex-col justify-center items-center px-6 md:px-20'>
             {children}
         </div>
     );
