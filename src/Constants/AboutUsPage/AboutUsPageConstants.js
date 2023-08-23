382c8652f6408d2e939e3cab395b2715679dd15b
unknown <mirajulislamofficial044@gmail.com>
2023-08-23 16:50:41 +0600

    why choose us mock added

--- src/Constants/AboutUsPage/AboutUsPageConstants.js
+++ src/Constants/AboutUsPage/AboutUsPageConstants.js
@@ -1,3 +1,8 @@
+import CollaborativeApproach from "../../Assets/Aboutus/Icons/Collaborative Approach.svg";
+import Communication from "../../Assets/Aboutus/Icons/Communication.svg";
+import Expertise from "../../Assets/Aboutus/Icons/Expertise.svg";
+import TailoredSolutions from "../../Assets/Aboutus/Icons/Tailored Solutions.svg";
+
 export const AboutUsHeroConstants = {
   title: "Welcome to Resdium: Crafting   Digital Excellence",
   desc: "At Resdium, we`re not just another digital agency; we`re your dedicated partner in harnessing the power of the online world.   Weunderstand that in today`s fast-paced business landscape, a   strong online presence is no longer a luxury – it`s a necessity. That`s   why we`re here to transform your ideas into digital realities that   drive success.",
@@ -22,5 +27,32 @@ export const AboutUsConstants = [
   {
     title: "Our Commitment",
     desc: "At Resdium, our commitment to quality is unwavering. We believe that  every pixel, every line of code, and every interaction matter. Our dedicated  team pours heart and soul into each project, ensuring that every website we craft is a masterpiece. We take pride in our attention to detail, our pursuit  of perfection, and our dedication to delivering results that exceed expectations.",
+  },
+];
+
+export const AboutUs = [
+  {
+    title: "Expertise",
+    icon: Expertise,
+    alt: "expertise-icon",
+    desc: "Our team boasts a wealth of experience and expertise, ensuring      that your project is in capable hands.",
+  },
+  {
+    title: "Tailored Solutions",
+    icon: TailoredSolutions,
+    alt: "tailored-solutions-icon",
+    desc: "We don't believe in one-size-fits-all. Our solutions are tailor-made to address your unique needs and challenges.",
+  },
+  {
+    title: "Communication",
+    icon: Communication,
+    alt: "communication-icon",
+    desc: "We value open communication and transparency. We keep you in the loop at every stage of development.",
+  },
+  {
+    title: "Collaborative Approach",
+    icon: CollaborativeApproach,
+    alt: "collaborativeapproach-icon",
+    desc: "We see our clients as collaborators. Your insights fuel our creativity, resulting in projects that truly reflect your vision.",
   },
 ];
