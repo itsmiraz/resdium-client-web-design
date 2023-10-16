0c99e1c1bc2a2ac925ab13c6b4892ede0e3116fa
unknown <mirajulislamofficial044@gmail.com>
2023-10-16 16:36:13 +0600

    new

--- src/Components/BlogPage/BlogCard/BlogCard.jsx
+++ src/Components/BlogPage/BlogCard/BlogCard.jsx
@@ -22,7 +22,7 @@ const BlogCard = ({ blog }) => {
             </div>
 
             <div className='p-4 relative shadow-inner ' >
-                <p className='text-sm'>Blog by <Link className='hover:underline' href={'https//:resdium.live'}>Resdium Digital</Link></p>
+                <p className='text-sm'>Blog by <Link className='hover:underline' href={'/'}>Resdium Digital</Link></p>
                 <h1 className='text-2xl font-semibold'>{title.length > 42 ? `${title.slice(0, 42)}..` : title}</h1>
                 <p className='text-[#666] text-[16px]  lg:leading-[20px] leading-[18px] md:leading-[20px] font-medium py-4'>
                     {outline.slice(0, 200)}..
