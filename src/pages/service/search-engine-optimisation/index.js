0558fe843d4ce958179de274a70384e31a4f204f
itsmiraz <mirajulislamofficial044@gmail.com>
2023-11-21 21:13:39 +0600

    meta tags added

--- src/pages/service/search-engine-optimisation/index.js
+++ src/pages/service/search-engine-optimisation/index.js
@@ -10,7 +10,34 @@ const Seo = () => {
   return (
     <main>
       <Head>
-        <title>Search Engine Optimisation - Resdium Digital</title>
+        <title>Search Engine Optimization - Resdium Digital</title>
+        <meta
+          name="description"
+          content="Resdium Digital offers top-notch Search Engine Optimization (SEO) services to elevate your online presence. Partner with us for tailored strategies that boost your landscaping business in today's competitive digital market."
+        />
+        <meta
+          property="og:title"
+          content="Search Engine Optimization - Resdium Digital"
+        />
+        <meta
+          property="og:description"
+          content="Resdium Digital offers top-notch Search Engine Optimization (SEO) services to elevate your online presence. Partner with us for tailored strategies that boost your landscaping business in today's competitive digital market."
+        />
+
+        <meta
+          property="og:url"
+          content="https://resdium.live/service/search-engine-optimisation"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/djnlyzsmv/image/upload/v1700547478/resdium-assets/Service%20Page/seo-hero-resdium-digital-min_vkvy8z.webp"
+        />
+        <link
+          rel="canonical"
+          href="https://resdium.live/service/search-engine-optimisation"
+        />
+
+        <meta property="og:type" content="website" />
       </Head>
 
       <SeoHero />
