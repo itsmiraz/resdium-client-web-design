e9f44dd1201f77e5bed0a9960800a075e50db92b
itsmiraz <mirajulislamofficial044@gmail.com>
2024-03-02 16:33:54 +0600

    new section added

---
+++ src/Constants/WebDesignInUtah/index.js
@@ -0,0 +1,18 @@
+export const WebDesignInUtahExpertise = [
+  {
+    title: "User-Centric Design",
+    desc: "Crafting websites that prioritize user experience, ensuring seamless navigation and engagement for visitors.",
+  },
+  {
+    title: "Mobile Optimization",
+    desc: "Specializing in responsive design to guarantee optimal viewing and interaction experiences across all devices.",
+  },
+  {
+    title: "Conversion-Driven Strategy",
+    desc: "Implementing strategies to maximize conversions and achieve business objectives, from lead generation to sales.",
+  },
+  {
+    title: "Cutting-Edge Technologies",
+    desc: "Leveraging the latest tools and technologies to create visually stunning and technically robust websites that stand out in the digital landscape.    ",
+  },
+];
--- src/pages/location/web-design-in-utah/index.js
+++ src/pages/location/web-design-in-utah/index.js
@@ -1,12 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
 // eslint-disable-next-line react/no-unescaped-entities
+import { WebDesignInUtahExpertise } from "@/Constants/WebDesignInUtah";
 import Head from "next/head";
 import Image from "next/image";
 import Balancer from "react-wrap-balancer";
+import expertiseIcon from "../../../../public/Assets/Icons/expertise.png";
 
 const WebDesignInUtah = () => {
   return (
-    <main>
+    <main className="px-4 md:px-20 ">
       <Head>
         <title>
           Web Design in Utah | Grow Your Business Online - Resdium Digital
@@ -27,6 +29,7 @@ const WebDesignInUtah = () => {
         />
       </Head>
 
+      {/* Hero */}
       <div className="py-20">
         <h1 className="text-[56px] text-center leading-[100%] text-[#22323F] font-bold">
           Utah's Trusted Web <br /> Design Partner: Grow Your <br /> Business
@@ -49,6 +52,58 @@ const WebDesignInUtah = () => {
           }
           alt="utah-image"
         />
+      </div>
+      {/* Expertise */}
+      <div className="grid grid-cols-2 gap-10">
+        <div>
+          <p className="text-lg font-semibold">Expertise</p>
+          <h2 className="text-[48px] font-bold">
+            Why Choose Resdium Digital for Web Design in Utah?
+          </h2>
+        </div>
+        <div className="space-y-6  py-20">
+          {WebDesignInUtahExpertise.map((item, key) => (
+            <div className="flex items-center" key={key}>
+              <div className="flex flex-col justify-center items-center gap-2">
+                <Image src={expertiseIcon} alt="expetise" />
+                <div className="h-[70px] w-[3px] bg-gray-700"></div>
+              </div>
+              <div className="p-6">
+                <h3 className="text-2xl font-semibold">{item.title}</h3>
+                <p className="text-medium">{item.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Unleash the Potential of Your Online Presence */}
+      <div className="grid py-20 place-content-center place-items-center  grid-cols-2 gap-10">
+        <div>
+          <h2 className="text-[48px] leading-[110%] font-bold">
+            Unleash the Potential of Your Online Presence
+          </h2>
+          <p className="text-lg mt-4">
+            We offer a comprehensive suite of digital marketing services to help
+            you reach your target audience, engage them effectively, and achieve
+            your business goals. Here's how we can help:
+          </p>
+          <ul className="list-disc pl-10 space-y-4 mt-4 text-semibold text-2xl font-semibold">
+            <li>Web Development and Design</li>
+            <li>SEO Services</li>
+            <li>Social Media Content Creation</li>
+          </ul>
+        </div>
+        <div>
+          <Image
+            width={400}
+            height={400}
+            className="w-full h-full"
+            src={
+              "https://res.cloudinary.com/djnlyzsmv/image/upload/v1709373881/resdium-assets/location-based-pages/Unleash_the_Potential_of_Your_Online_Presence_ke6sh2.webp"
+            }
+            alt="unleash-the-potential-of-your-online-presence-cover"
+          />
+        </div>
       </div>
     </main>
   );
