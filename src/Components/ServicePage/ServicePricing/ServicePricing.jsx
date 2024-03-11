fb8c235f39f978ae7958f0d774d082dfeddf7d88
itsmiraz <mirajulislamofficial044@gmail.com>
2024-03-11 08:57:09 +0600

    pricing removed

--- src/Components/ServicePage/ServicePricing/ServicePricing.jsx
+++ src/Components/ServicePage/ServicePricing/ServicePricing.jsx
@@ -7,21 +7,9 @@ import Balancer from 'react-wrap-balancer';
 
 const ServicePricing = () => {
 
-    const [userLocation, setUserLocation] = useState({});
 
-    useEffect(() => {
-        async function fetchGeolocation() {
-            const geolocation = await getUserGeolocation();
-            setUserLocation(geolocation);
-        }
-        fetchGeolocation();
-    }, []);
-    // Function to determine if the user is in India based on the geolocation data
-    const isUserIsINbd = () => {
-        return userLocation && userLocation.country === "BD";
-    };
 
-    console.log(isUserIsINbd());
+
 
 
     const PackageCard = ({ data }) => {
@@ -44,7 +32,7 @@ const ServicePricing = () => {
                         }
                     </ul>
                 </div>
-                <div className='flex flex-wrap items-center  pl-4 text-start justify-between lg:justify-around'>
+                {/* <div className='flex flex-wrap items-center  pl-4 text-start justify-between lg:justify-around'>
                     <div className=' flex md:flex-col  flex-wrap items-center gap-4 w-full md:w-fit justify-between '>
                         <div>
                             <h2 className='font-semibold text-sm'>Starts With</h2>
@@ -85,8 +73,11 @@ const ServicePricing = () => {
                             </div>
                         </>
                     }
-                </div>
+                </div> */}
+                <div className='pl-10 text-start '>
+                    <Link href={'/contactUs'}> <button className='bg-white text-sm  rounded-ss-[6px] rounded-ee-[6px] text-gray-800 font-semibold px-4 py-2'>Get Started </button></Link>
 
+                </div>
 
             </div>
         )
@@ -96,50 +87,49 @@ const ServicePricing = () => {
         <div>
 
             {/* {isUserIsINbd() ? '' : */}
-                <section className='px-6 lg:px-20 py-20'>
-                    <div className='text-center'>
-                        <h1 className='text-[#22323F] text-[26px] lg:text-[48px] leading-[28px] lg:leading-[52px]  font-bold'>
+            <section className='px-6 lg:px-20 py-20'>
+                <div className='text-center'>
+                    <h1 className='text-[#22323F] text-[26px] lg:text-[48px] leading-[28px] lg:leading-[52px]  font-bold'>
 
-                            Our Web Development
-                            <br /> Service Pricing
-                        </h1>
+                        Our Web Development
+                        <br /> Service Pricing
+                    </h1>
 
 
 
-                        <div className='flex lg:flex-row flex-col justify-center gap-10 items-start pt-20'>
-                            <div className='grid grid-cols-1 md:grid-cols-2 gap-10 lg:gap-10'>
-                                <div className='md:col-span-1 h-full col-span-2'>
-                                    <PackageCard data={servicePagePackage.bronzePacakge} />
-                                </div>
-                                <div className='md:col-span-1 h-full col-span-2'>
-                                    <PackageCard data={servicePagePackage.SilverPacakge} />
-                                </div>
+                    <div className='flex lg:flex-row flex-col justify-center gap-10 items-start pt-20'>
+                        <div className='grid grid-cols-1 md:grid-cols-2 gap-10 lg:gap-10'>
+                            <div className='md:col-span-1 h-full col-span-2'>
+                                <PackageCard data={servicePagePackage.bronzePacakge} />
+                            </div>
+                            <div className='md:col-span-1 h-full col-span-2'>
+                                <PackageCard data={servicePagePackage.SilverPacakge} />
+                            </div>
 
-                                <div className='bg-[#284A62] hidden rounded-ss-[20px] rounded-ee-[20px] lg:flex justify-between items-center px-6 py-10 w-full h-full col-span-2'>
-                                    <h1 className='text-white text-[32px] font-semibold'>
-                                        Dont know where to start?
-                                    </h1>
-                                    <div>
-                                        <Link href={'/contactUs'}> <button className='bg-white rounded-ss-[6px] rounded-ee-[6px] text-gray-800 font-semibold px-4 py-2'> Schedule A Call </button></Link>
-                                    </div>
+                            <div className='bg-[#284A62] hidden rounded-ss-[20px] rounded-ee-[20px] lg:flex justify-between items-center px-6 py-10 w-full h-full col-span-2'>
+                                <h1 className='text-white text-[32px] font-semibold'>
+                                    Dont know where to start?
+                                </h1>
+                                <div>
+                                    <Link href={'/contactUs'}> <button className='bg-white rounded-ss-[6px] rounded-ee-[6px] text-gray-800 font-semibold px-4 py-2'> Schedule A Call </button></Link>
                                 </div>
                             </div>
-                            <PackageCard data={servicePagePackage.PREMIUMPacakge} />
                         </div>
-                        <div className='bg-[#284A62] lg:hidden rounded-ss-[20px] rounded-ee-[20px] mt-10 flex-col justify-between items-center px-6 py-8 space-y-6 w-full h-full col-span-2'>
-                            <h1 className='text-white text-[28px] leading-[32px] font-semibold'>
-                                Dont know where to start?
-                            </h1>
-                            <div>
-                                <Link href={'/contactUs'}> <button className='bg-white rounded-ss-[6px] rounded-ee-[6px] text-gray-800 font-semibold px-4 py-2'> Schedule A Call </button></Link>
-                            </div>
-                        </div>
-
+                        <PackageCard data={servicePagePackage.PREMIUMPacakge} />
                     </div>
-                </section>
-            
-            
-            {/* // } */}
+                    <div className='bg-[#284A62] lg:hidden rounded-ss-[20px] rounded-ee-[20px] mt-10 flex-col justify-between items-center px-6 py-8 space-y-6 w-full h-full col-span-2'>
+                        <h1 className='text-white text-[28px] leading-[32px] font-semibold'>
+                            Dont know where to start?
+                        </h1>
+                        <div>
+                            <Link href={'/contactUs'}> <button className='bg-white rounded-ss-[6px] rounded-ee-[6px] text-gray-800 font-semibold px-4 py-2'> Schedule A Call </button></Link>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+
 
 
         </div>
